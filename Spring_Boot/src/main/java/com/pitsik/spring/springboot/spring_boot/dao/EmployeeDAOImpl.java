94b2874abc13639ce0a9a314d268d39c3123f151
apicik <apicik@ya.ru>
2021-09-02 23:35:47 +0400

    complete, use hibernate class

--- Spring_Boot/src/main/java/com/pitsik/spring/springboot/spring_boot/dao/EmployeeDAOImpl.java
+++ Spring_Boot/src/main/java/com/pitsik/spring/springboot/spring_boot/dao/EmployeeDAOImpl.java
@@ -2,6 +2,7 @@ package com.pitsik.spring.springboot.spring_boot.dao;
 
 import com.pitsik.spring.springboot.spring_boot.entity.Employee;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -27,28 +28,31 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         return allEmployees;
     }
 
-//    @Override
-//    public void saveEmployee(Employee employee) {
+    @Override
+    public void saveEmployee(Employee employee) {
 //        Session session = sessionFactory.getCurrentSession();
-//
-//        session.saveOrUpdate(employee);
-//    }
-//
-//    @Override
-//    public Employee getEmployee(int id) {
+        Session session = entityManager.unwrap(Session.class);
+
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
 //        Session session = sessionFactory.getCurrentSession();
-//
-//        Employee employee = session.get(Employee.class, id);
-//        return employee;
-//    }
-//
-//    @Override
-//    public void deleteEmployee(int id) {
+        Session session = entityManager.unwrap(Session.class);
+
+        Employee employee = session.get(Employee.class, id);
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
 //        Session session = sessionFactory.getCurrentSession();
-//
-//        Query<Employee> query = session.createQuery("delete from Employee where id =:employeeId");
-//
-//        query.setParameter("employeeId", id);
-//        query.executeUpdate();
-//    }
+        Session session = entityManager.unwrap(Session.class);
+
+        Query<Employee> query = session.createQuery("delete from Employee where id =:employeeId");
+
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+    }
 }
